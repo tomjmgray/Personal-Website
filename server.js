8cc8714f666ba51f2ffb393136448a65e0bd0963
tomjmgray <tomjmgray96@gmail.com>
2021-01-26 10:46:54 -0800

    Added a bunch of CSS to homepage, demo of projects page

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home.ejs');
+    res.render('home');
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
 })
 
 app.set('view engine', 'ejs');
